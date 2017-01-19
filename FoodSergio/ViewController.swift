eb244595628f879cec365446a13dc71f18f386f9
dam <izv.dam.nono@gmail.com>
2017-01-19 09:27:00 +0100

    Initial Commit

---
+++ FoodSergio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodSergio
+//
+//  Created by dam on 19/1/17.
+//  Copyright © 2017 dam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
